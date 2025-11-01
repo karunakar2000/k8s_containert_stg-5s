68d8785f57404d6801f423442ae8f8a84f521d94
karunakar2000 <karunakar2000@gmail.com>
2025-11-01 16:41:11 +0200

    module_update

--- terraform_vpc/vpc.tf
+++ terraform_vpc/vpc.tf
@@ -137,3 +137,15 @@ resource "aws_nat_gateway" "natgw" {
   # on the Internet Gateway for the VPC.
   depends_on = [aws_internet_gateway.main]
 }
+
+resource "aws_route" "private" {
+  route_table_id            = aws_route_table.private.id
+  destination_cidr_block    = "0.0.0.0/0"
+  nat_gateway_id = aws_nat_gateway.natgw.id
+}
+
+resource "aws_route" "database" {
+  route_table_id            = aws_route_table.database.id
+  destination_cidr_block    = "0.0.0.0/0"
+  nat_gateway_id = aws_nat_gateway.natgw.id
+}
