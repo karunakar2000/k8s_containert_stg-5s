5e51143efbe1d53bbe85895d07bf96ba7876082e
karunakar2000 <karunakar2000@gmail.com>
2025-11-01 18:55:26 +0200

    module_update

--- terraform_vpc/vpc.tf
+++ terraform_vpc/vpc.tf
@@ -158,12 +158,12 @@ resource "aws_route_table_association" "public" {
 
 resource "aws_route_table_association" "private" {
   count = length(var.private_subnet_cidrs)
-  subnet_id      = aws_subnet.public[count.index].id
+  subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private.id
 }
 
 resource "aws_route_table_association" "database" {
   count = length(var.database_subnet_cidrs)
-  subnet_id      = aws_subnet.public[count.index].id
-  route_table_id = aws_route_table.private.id
+  subnet_id      = aws_subnet.database[count.index].id
+  route_table_id = aws_route_table.database.id
 }
